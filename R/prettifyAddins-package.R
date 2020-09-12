92512e63438af059ac00a9b4584a4cf479e24253
stla <laurent_step@yahoo.fr>
2020-09-12 12:18:08 +0200

    file prettifyAddins-package.R

---
+++ R/prettifyAddins-package.R
@@ -0,0 +1,11 @@
+#' Prettify Addins
+#'
+#' This package provides two RStudio addins: \code{Prettify} and \code{Indent}.
+#' To run an addin, select \code{Prettify} or \code{Indent} from the Addins
+#' menu within RStudio. The \code{Indent} addin only reindents the code, while
+#' the \code{Prettify} addin also modifies the code, e.g. it adds trailing
+#' semi-colons to JavaScript code when they are missing.
+#'
+#' @name prettifyAddins
+#' @docType package
+NULL
