a1aa8e9b13a0657c829c5ef228178c423f9a0c58
stla <laurent_step@yahoo.fr>
2020-10-18 04:21:50 +0200

    prettify XML

--- R/prettifyXML.R
+++ R/prettifyXML.R
@@ -1,28 +1,66 @@
-#' @importFrom rstudioapi getSourceEditorContext setDocumentContents
+#' @importFrom rstudioapi isAvailable
 #' @importFrom tools file_ext
 #' @importFrom xml2 read_xml
-NULL
-
-prettifyXML <- function(){
-  context <- getSourceEditorContext()
-  ext <- tolower(file_ext(context[["path"]]))
-  if(!is.element(ext, c("xml", "svg"))){
-    message("Looks like this file is not a XML file.")
+prettifyXML <- function(contents = NA, tabSize = NULL){
+  if(is.na(contents) && isAvailable()){
+    context <- RStudioContext()
+    ext <- tolower(file_ext(context[["path"]]))
+    if(!is.element(ext, c("xml", "svg"))){
+      message("Looks like this file is not a XML file.")
+      return(invisible())
+    }
+    if(is.null(tabSize)){
+      tabSize <- RStudioTabSize()
+    }
+    contents <- context[["contents"]]
+  }else if(is.na(contents)){
+    message("You have to provide something for the `contents` argument.")
     return(invisible())
-  }
-  code <- paste0(context[["contents"]], collapse = "\n")
-  prettyCode <- tryCatch({
-    as.character(read_xml(code))
-  }, error = function(e){
-    NULL
-  })
-  if(is.null(prettyCode)){
-    message(
-      "Something went wrong. ",
-      "Probably the code is not valid."
-    )
   }else{
-    setDocumentContents(text = prettyCode, id = context[["id"]])
+    if(is.null(tabSize)){
+      if(isAvailable()){
+        tabSize <- RStudioTabSize()
+      }else{
+        tabSize <- 2
+      }
+    }
+    if(file.exists(contents)){
+      contents <- suppressWarnings(readLines(contents))
+    }
   }
-  return(invisible())
+  if(Sys.which("xmllint") == ""){
+    message(
+      "`xmllint` not found --- using `xml2` (ignoring `tabSize`)"
+    )
+    prettyCode <- tryCatch({
+      as.character(read_xml(paste0(contents, collapse = "\n")))
+    }, error = function(e){
+      NULL
+    })
+    if(is.null(prettyCode)){
+      message(
+        "Something went wrong. ",
+        "Probably the code is not valid."
+      )
+      return(invisible())
+    }
+  }else{
+    tmpFile <- tempfile(fileext = ".xml")
+    writeLines(contents, tmpFile)
+    Sys.setenv(XMLLINT_INDENT = tabSize)
+    prettyCode <- suppressWarnings(system2(
+      "xmllint", paste0("--format ", tmpFile), stdout = TRUE, stderr = TRUE
+    ))
+    if(!is.null(attr(prettyCode, "status"))){
+      message(
+        "Something went wrong. ",
+        "Probably the code is not valid."
+      )
+      return(invisible())
+    }
+  }
+
+  prettyCode
 }
+
+# todo: ligne de commande pour python? on verra RStudio 1.4
